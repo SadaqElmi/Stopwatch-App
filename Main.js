bd298c81ed91857c0868f04996535b7687df541f
SadaqElmi <hooymacaansadaq@mail.com>
2024-04-26 03:32:17 +0300

    modified:   Main.js

--- Main.js
+++ Main.js
@@ -1,16 +1,25 @@
-const startBtn = document.querySelector(".startBtn");
-const StopBtn = document.querySelector(".stopBtn");
-const restBtn = document.querySelector(".resetBtn");
-
-startBtn.addEventListener("click", start);
-StopBtn.addEventListener("click", stop);
-restBtn.addEventListener("click", stopTime);
-
 let second = 0;
 let minutes = 0;
 let hour = 0;
 let isRunning = false;
 let interval;
+
+const startBtn = document.querySelector(".startBtn");
+const stopBtn = document.querySelector(".stopBtn");
+const resetBtn = document.querySelector(".resetBtn");
+
+startBtn.addEventListener("click", start);
+stopBtn.addEventListener("click", stop);
+resetBtn.addEventListener("click", reset);
+
+// Check if there is a saved time in localStorage
+if (localStorage.getItem("stopwatchTime")) {
+  const savedTime = JSON.parse(localStorage.getItem("stopwatchTime"));
+  hour = savedTime.hour;
+  minutes = savedTime.minutes;
+  second = savedTime.second;
+  display();
+}
 
 function start() {
   if (!isRunning) {
@@ -24,7 +33,8 @@ function updateTime() {
   if (second === 60) {
     second = 0;
     minutes++;
-  } else if (minutes === 60) {
+  }
+  if (minutes === 60) {
     minutes = 0;
     hour++;
   }
@@ -34,25 +44,35 @@ function updateTime() {
 function stop() {
   clearInterval(interval);
   isRunning = false;
+  // Save the current time to localStorage
+  saveTime();
 }
 
-function stopTime() {
+function reset() {
   stop();
-  let second = 0;
-  let minutes = 0;
-  let hour = 0;
-  let isRunning = false;
+  second = 0;
+  minutes = 0;
+  hour = 0;
   display();
+  // Clear the saved time from localStorage
+  localStorage.removeItem("stopwatchTime");
 }
 
 function display() {
   const displayBtn = document.querySelector(".display");
-
   displayBtn.textContent = `${formatTime(hour)}:${formatTime(
     minutes
   )}:${formatTime(second)}`;
 }
 
 function formatTime(time) {
-  return time < 10 ? `0${time} ` : time;
+  return time < 10 ? `0${time}` : time;
+}
+
+function saveTime() {
+  // Save the current time to localStorage
+  localStorage.setItem(
+    "stopwatchTime",
+    JSON.stringify({ hour: hour, minutes: minutes, second: second })
+  );
 }
